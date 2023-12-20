d2c1e2c2a7237854faeb1715c2ebed08e08df6d4
Jorge Caloir <47183518+Jorgecaloir1992@users.noreply.github.com>
2023-12-19 23:18:03 -0300

    loops

---
+++ Java/src/loops.java
@@ -0,0 +1,12 @@
+public class loops {
+    public static void main(String[] args) {
+    int i = 0;
+   int lap = 0;
+    while (i < 10) {
+        lap += 1;
+        System.out.println(i + ", lap: " + lap);
+        i += 2;
+        
+    }
+    }
+}
